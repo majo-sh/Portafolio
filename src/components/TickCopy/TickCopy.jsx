354aa7d1a9fe3e324ab9422fc2a74d50abba2759
majo-sh <majo.cortes.sh@gmail.com>
2022-02-21 17:04:17 -0500

    arreglos para mobil de ticktock

--- src/components/TickCopy/TickCopy.jsx
+++ src/components/TickCopy/TickCopy.jsx
@@ -1,11 +1,11 @@
 import './TickCopy.css'
 
-export default function TickCopy () {
+export default function TickCopy() {
     return (
-        <aside className ="container-tick">
+        <aside className="container-tick">
             <div className="tick-back">
-                <div className="tick-front"> 
-                    <span className="tick-text">T<br/>i<br/>c<br/>k<br/>T<br/>o<br/>c<br/>k</span>
+                <div className="tick-front">
+                    <span className="tick-text">T<br />i<br />c<br />k<br />T<br />o<br />c<br />k</span>
                 </div>
             </div>
         </aside>
